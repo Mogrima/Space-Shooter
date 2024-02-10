90aebd4b00f90089c1690cbc7211cafedda1108c
Mogrima <rebel.rs@yandex.ru>
2024-02-10 07:57:50 +0500

    создан главный игровой класс, в нем реализован метод resize, который запускается каждый раз при изменений события resize и изменяет размеры canvas

---
+++ src/Game.js
@@ -0,0 +1,19 @@
+export class Game {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+
+        window.addEventListener('resize', e => {
+            this.resize(e.target.innerWidth,
+                e.target.innerHeight);
+        });
+    }
+
+    resize(width, height) {
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.width = width;
+        this.height = height;
+    }
+}
